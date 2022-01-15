80466bb302122a864c141b5b6333e3b0fb8a4737
BastienVanhove <bastienv.capital@gmail.com>
2022-01-15 10:31:03 -0500

    first commit

---
+++ JS/objectObserver/readme.md
@@ -0,0 +1,13 @@
+## for use
+    ``` javascript
+    let object = {x: 0, y: 0}
+    let func = (target, key, value) => {
+        console.log('variable change')
+        console.log(`target : ${target}, key : ${key}, value ${value}`);
+    }
+
+    let variableChangeur = objectListener(object, func) 
+    object.z = 7
+
+    console.log(object)
+    ```
---
+++ JS/objectObserver/script.js
@@ -0,0 +1,20 @@
+const objectListener = (targetObj, doSomething) => {
+    return new Proxy(targetObj, {
+        set: function (target, key, value) {
+            doSomething(target, key, value);
+            target[key] = value;
+            return true;
+        }
+      });
+}
+
+let object = {x: 0, y: 0}
+let func = (target, key, value) => {
+    console.log('variable change')
+    console.log(`target : ${target}, key : ${key}, value ${value}`);
+}
+
+let variableChangeur = objectListener(object, func) 
+object.z = 7
+
+console.log(object)
